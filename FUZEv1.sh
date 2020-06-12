dfe05c7b206d547047dac9a9cbda0123539670ec
MGithubz <66805907+MGithubz@users.noreply.github.com>
2020-06-12 00:06:25 +0000

    Update FUZEv1.sh

--- FUZEv1.sh
+++ FUZEv1.sh
@@ -1,39 +1,6 @@
 #!/bin/bash
 #
-# FUZE! [version.1]
-#
-# An efficient script made by ME to speed shit up.
-# The " root@D0loresH4ze:~/FUZE.sh# " part is editable, it's just cosmetics.
-# For example you can put "Choose an Option: " Instead of that and the script will still work.
-# Just make sure to only edit the text in between the apostrophes ( ' ' ) AND do NOT edit the " PS3= " part or the script may not work.
-# =====================================================================================================================================
-# To ADD your OWN options to this script, copy this: 
-#
-# ===========================================
-#
-#         "YourOption")
-#             clear && YourCommand
-#             ;;
-#
-# ===========================================
-#
-# And put it underneath or above another option's double semicolons Like this:
-#
-# ===========================================
-#
-#         "YourOption")
-#             clear && YourCommand
-#             ;;
-#         "AnotherOption")
-#             clear && AnotherCommand
-#             ;;
-# 
-# ===========================================
-#
-# After that, add "YourOption" to the " options=( " part under " PS3=' " with a single space from right and left between the other options.
-# Make sure that your new option is in the right order with the others, otherwise the script will not work.
-#==========================================================================================================================================
-
+# FUZE.sh [version.1]
 PS3='root@D0loresH4ze:~/FUZE.sh# '
 options=("Monitor ON" "Monitor OFF" "ifconfig wlan0mon down" "ifconfig wlan0mon up" 
 "ifconfig wlan0 down" "ifconfig wlan0 up" "Set custom MAC" "Set random MAC" "Reset MAC" 
